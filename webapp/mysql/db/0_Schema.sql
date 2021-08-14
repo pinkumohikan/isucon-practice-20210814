8069b8943b34df38b4ea2e807ed5ebf302c407a4
cureseven <shiningcureseven@gmail.com>
2021-08-14 13:44:25 +0900

    if not exists

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -26,7 +26,7 @@ alter table isuumo.estate add index (door_height, popularity);
 alter table isuumo.estate add index (latitude);
 alter table isuumo.estate add index (longitude);
 alter table isuumo.estate add index (popularity);
-alter table isuumo.estate add column minus_popularity int as (popularity * -1) stored not null;
+alter table if not exists isuumo.estate add column minus_popularity int as (popularity * -1) stored not null;
 alter table isuumo.estate add index (minus_popularity, id);
 
 CREATE TABLE isuumo.chair
