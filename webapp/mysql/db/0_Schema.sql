d0a6c2237aa0f68c19612515e158fc3922d51ede
masami <msihima0304@gmail.com>
2021-08-14 15:31:30 +0900

    feat: 🎸 検索クエリ高速化

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -24,6 +24,8 @@ alter table isuumo.estate add index (rent, id);
 alter table isuumo.estate add index (rent, popularity, id);
 alter table isuumo.estate add index (door_width, popularity);
 alter table isuumo.estate add index (door_height, popularity);
+alter table isuumo.estate add index (rent, door_width);
+alter table isuumo.estate add index (rent, door_height);
 alter table isuumo.estate add index (latitude);
 alter table isuumo.estate add index (longitude);
 alter table isuumo.estate add index (popularity);
