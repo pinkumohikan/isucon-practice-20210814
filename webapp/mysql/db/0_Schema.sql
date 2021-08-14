802c54731f63230fe1156b8c1f1305550e64e6e4
masami <msihima0304@gmail.com>
2021-08-14 17:22:55 +0900

    feat: 🎸 indexいい感じ

--- webapp/mysql/db/0_Schema.sql
+++ webapp/mysql/db/0_Schema.sql
@@ -25,6 +25,7 @@ CREATE TABLE isuumo.estate
 );
 alter table isuumo.estate add index (rent, id);
 alter table isuumo.estate add index (rent, popularity, id);
+alter table isuumo.estate add index (door_height_range_id, door_width_range_id,rent_range_id);
 alter table isuumo.estate add index (door_width_range_id, popularity);
 alter table isuumo.estate add index (door_height_range_id, popularity);
 alter table isuumo.estate add index (rent_range_id, popularity);
